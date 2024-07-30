316cfbf694002bed9ed6812227cb291d247125d2
Yash <yashwagh7095@gmail.com>
2024-07-30 13:45:48 +0530

    commit

--- 22.cpp
+++ 22.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
+#include<string>
 using namespace std;
+class binary {
+    string s;
+    public :
+    void read (void);
+    void chk_binary(void);
+    void onces(void);
 
+};
+void binary :: read (void){
+    cout<<"enter the binary number "<<endl;
+    cin>>s;
+}
+void binary :: chk_binary(void){
+    for(int i = 0; i < s.length(); i++){
+        if(s.at(i)!='0' && s.at(i)!='1'){
+            cout<<"incorrect binary format "<<endl;
+            exit(0);
+        }
+    }
+}
+void binary :: onces (void){
+for(int i = 0; i < s.length(); i++){
+        if(s.at(i)+='0'){
+            s.at(i) = 1;
+        } 
+        if( s.at(i)=='1'){
+            s.at(i) = 0;
+}
 int main(){
 // OOPs - Classes and the Objects
 //  C++ --> initially called --> c with classes by stroutroup
@@ -14,8 +42,21 @@ int main(){
 // structure in c++ are typedefed
 // you can declare object along with the class declarion
 
-class employee{
+/*class employee{
 //class definition
-};
+}yash, karan, hemant;*/
+// yash.salary = 8 makes no sense if salary is private
+// nesting of member functions
+
+
+// };
+
+binary b;
+b.read();
+b.chk_binary();
+b.onces
+
+
 return 0;
+
 }
