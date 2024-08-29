ddc5a928d93b29e5cac490666bf898210031be2a
Yash <yashwagh7095@gmail.com>
2024-08-29 11:37:14 +0530

    commit  all

--- Lecture/30.cpp
+++ Lecture/30.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 class complex{
+    
     int a,b;
+    public:
+    complex(int ,int );
+    void printNumber(){
+        cout<<"Your number is "<<a<<"+"<<b<<"i"<<endl;
+    }
 };
-int main(){
+complex::complex(int x, int y){// this is a parameterized constructor it takes no parameters
 
+    a=x;
+    b=y;
+    
+}
+int main(){
+    //implicit call
+complex a(4,5);
+
+//Exlicit call
+complex b = complex(5,7);
+a.printNumber();
+b.printNumber();
 return 0;
 }
---
+++ Lecture/31.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Point{
+    int x, int y;
+    
+    public:
+    Point (int x, int y ){
+        a=x;
+        b=y;
+
+    }
+
+};
+
+int main(){
+
+return 0;
+}
