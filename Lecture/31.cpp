32e9f863bdeb88e2d28fa088d83b291502910c75
Yash <yashwagh7095@gmail.com>
2024-08-29 12:19:55 +0530

    commit  all

--- Lecture/31.cpp
+++ Lecture/31.cpp
@@ -1,18 +1,43 @@
+//Multiple constructer in one program 
+
 #include<iostream>
 using namespace std;
-class Point{
-    int x, int y;
-    
+class Complex{
+    int a,b;
     public:
-    Point (int x, int y ){
+    
+    
+    Complex(){
+         a=0;
+         b=0;
+    }
+     Complex(int x, int y){
         a=x;
         b=y;
 
-    }
-
+     }
+     Complex(int x){
+        a = x;
+        b = 0;
+     }
+     void print(){
+        cout<<"Your complex no. is "<<a<< "+ "<<b<<"i "<<endl;
+     }
 };
 
+
 int main(){
+    Complex a(4,6);
+    a.print();
+    Complex b(5);
+    b.print();
+    Complex c;
+    c.print();
+    Complex d(2);
+    d.print();
+
+
+
 
 return 0;
 }
